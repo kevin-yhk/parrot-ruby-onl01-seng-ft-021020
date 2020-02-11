e3c5d2735f0c927b150e946432e332a660eebd9c
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-11 00:27:43 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(name = "Pretty bird!")
-  puts "Squawk!"
-  return "Squawk!"
+def parrot(string = "Squawk!")
+  puts "#{string}"
+  return 
   return name
 end
